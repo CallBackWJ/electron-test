823a6f6db034baac365978202205ab47b20bd839
1jin94 <1jin94@naver.com>
2020-03-03 15:09:29 +0900

    버전10

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
+
 let mainWindow;
 
-function createWindow() {
+function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
-
   mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     mainWindow = null;
   });
   mainWindow.once('ready-to-show', () => {
@@ -24,28 +24,30 @@ app.on('ready', () => {
   createWindow();
 });
 
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', function() {
+app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-ipcMain.on('app_version', event => {
+ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
 autoUpdater.on('update-available', () => {
   mainWindow.webContents.send('update_available');
 });
+
 autoUpdater.on('update-downloaded', () => {
   mainWindow.webContents.send('update_downloaded');
 });
+
 ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
 });
